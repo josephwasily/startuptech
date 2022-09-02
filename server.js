58bdb2b550824d500583183ebb5a4e93dd67cad8
Joseph Botros <jwasily@hotmail.com>
2022-09-02 16:25:30 +0300

    add changes

---
+++ server.js
@@ -0,0 +1,10 @@
+const { response } = require('express');
+let express = require('express');
+let app = express();
+app.use(express.static(__dirname+'/dist/startuptech'));
+
+app.get('/*', (req, res)=> {
+    res.sendFile(__dirname+'/dist/startuptech/index.html');
+});
+
+app.listen(process.env.PORT || 8080);
